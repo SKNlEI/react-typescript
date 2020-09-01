f4725c59896b01f2b8f342517f30ae8d77c6c6e1
DESKTOP-46BFAVA\skl <1907513952@qq.com>
2020-09-01 22:41:23 +0800

    feat: 新增文件别名

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -12,7 +12,20 @@ module.exports = {
     path: path.resolve(__dirname, '../dist')
   },
   resolve: {
-    extensions: [".ts", ".tsx", ".js", ".jsx"]
+    extensions: [".ts", ".tsx", ".js", ".jsx"],
+    alias: {
+      "@src": path.resolve(__dirname, '../src'),
+      "@views": path.resolve(__dirname, '../src/views/'),
+      "@server": path.resolve(__dirname, '../src/server/'),
+      "@assets": path.resolve(__dirname, '../src/assets/'),
+      "@hooks": path.resolve(__dirname, '../src/hokks/'),
+      "@components": path.resolve(__dirname, '../src/components/'),
+      "@constant": path.resolve(__dirname, '../src/constant/'),
+      "@global": path.resolve(__dirname, '../src/global/'),
+      "@router": path.resolve(__dirname, '../src/router/'),
+      "@store": path.resolve(__dirname, '../src/store/'),
+      "@utils": path.resolve(__dirname, '../src/utils')
+    }
   },
   devServer: {
     port: 9999,
