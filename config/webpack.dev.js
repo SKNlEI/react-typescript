2222b2830c6e103e4d8f94ca3f97173e47fd37c8
DESKTOP-46BFAVA\skl <1907513952@qq.com>
2020-09-01 21:38:32 +0800

    feat: 新增webpack.dev配置

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,4 +1,50 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
 module.exports = {
   mode: 'development',
-  entry: ''
+  entry: {
+    app: './src/index.tsx'
+  },
+  output: {
+    filename: '[name].[hash:8].js',
+    path: path.resolve(__dirname, '../dist')
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          "css-loader"
+        ]
+      },
+      {
+        test: /\.less$/,
+        use: [
+          "style-loader",
+          "css-loader",
+          "less-loader"
+        ]
+      },
+      {
+        test: /\.jsx?$/,
+        use: "babel-loader"
+      },
+      {
+        test: /\.tsx?$/,
+        use: "awesome-typescript-loader"
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'index.html',
+      inject: true
+    })
+  ]
 }
