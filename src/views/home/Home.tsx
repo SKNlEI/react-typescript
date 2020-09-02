974c849365dc203fd762ebef3c658e97bdda5179
DESKTOP-46BFAVA\skl <1907513952@qq.com>
2020-09-02 22:00:20 +0800

    feat：新增antd @babel/preset-env

--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Button } from 'antd'
 import './Home.less'
 
 function Home() {
   return (
     <div className="home">
       <h1>这个是home组件</h1>
+      <Button type="primary">添加</Button>
     </div>
   )
 }
